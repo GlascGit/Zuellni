d11ae715e80037d1fc2e2f20bdd388d5961ddef4
GlascGit <glascmain@protonmail.com>
2026-03-17 15:13:50 +0100

    Added robots and sitemap

--- W/script.js
+++ W/script.js
@@ -259,7 +259,21 @@
                 renderPage();
             }
         };
+        document.getElementById('menuBtn').addEventListener('click', function() {
+            document.getElementById('dropdownMenu').classList.toggle('show');
+        });
 
+        window.addEventListener('click', function(event) {
+            if (!event.target.matches('.menu-button')) {
+                var dropdowns = document.getElementsByClassName('dropdown-menu');
+                for (var i = 0; i < dropdowns.length; i++) {
+                    var openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        });
         // ---------------- INIT ----------------
 
         initDB().then(checkCoreUpdate);
